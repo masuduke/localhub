ab2c215ac8a395401ae0f5bcb582cd2c05d94739
Masud Rana <masud.uk.e@gmail.com>
2026-05-23 21:46:01 +0100

    disable minification to diagnose TDZ source

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.optimization.splitChunks = false
+      config.optimization.minimize = false
     }
     return config
   }
